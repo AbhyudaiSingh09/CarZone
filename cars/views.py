16540cb1a5470bf9bca678d318a5bf46266c4a3e
Abhyudai Singh <axs7941@mavs.uta.edu>
2023-02-22 02:52:51 -0600

    Carsetup_1

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import car
 
 
 def cars(request):
     return render(request, 'cars/cars.html')
+
+
+def car_detail(request,id):
+    single_car = get_object_or_404(car, pk=id)
+
+    data={
+        'single_car': single_car
+    }
+    return render(request,'cars/car_detail.html',data)
